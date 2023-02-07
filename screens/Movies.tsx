68e089343d6a6a2349b596221e18f20cb3b1665b
jeonghak <jeonghak.hur@gmail.com>
2023-02-07 17:22:12 +0900

    Moviews Screen part One

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text, TouchableOpacity } from "react-native";
-import styled from 'styled-components/native'
+import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import Swiper from "react-native-web-swiper";
+import styled from "styled-components/native";
 
-const Btn = styled.TouchableOpacity`
+const API_KEY = "384d29bebc04dc98585d34fedfd22ea6";
+
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+const View = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 `;
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.mainBgColor};
-`;
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation: { navigate },
-}) => (
-  <Btn onPress={() => navigate("Stack", { screen: "One" })}>
-    <Title>Movies</Title>
-  </Btn>
-);
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=384d29bebc04dc98585d34fedfd22ea6&language=en-US&page=1&region=KR`
+    );
+  };
+
+  return (
+    <Container>
+      <Swiper containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 export default Movies;
