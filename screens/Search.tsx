47572a285e34dc24e55c8372c334e5b4d1123fb7
jeonghak <jeonghak.hur@gmail.com>
2023-02-10 13:47:01 +0900

    search part one

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,9 +1,40 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { StyleSheet, SafeAreaView, TextInput } from "react-native";
+import { moviesApi } from "../api";
 
-const Movies = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Search</Text>
-  </View>
-);
-export default Movies;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  search_bar: {
+    borderWidth: 2,
+    borderRadius: 5,
+    fontSize: 20,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+  },
+});
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const handleChangText = (text: string) => setQuery(text);
+  console.log(query);
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        value={query}
+        style={styles.search_bar}
+        onChangeText={handleChangText}
+        placeholderTextColor="gray"
+        returnKeyType="search"
+        placeholder="Search"
+      />
+    </SafeAreaView>
+  );
+};
+export default Search;
